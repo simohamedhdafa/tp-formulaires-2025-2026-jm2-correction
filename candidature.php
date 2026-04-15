9d9bc65907b1c987a102eaa15edbcb2a93a726f6
hdafa <simohamd.hdafa@gmail.com>
2026-04-15 12:12:46 +0100

    feat: formulaire HTML avec tous les champs

--- candidature.php
+++ candidature.php
@@ -16,6 +16,32 @@
     <title>Correction TP formulaires</title>
 </head>
 <body>
+    <div class="container">
+        <form action="candidature.php" method="post">
+            <label>Prénom</label>
+            <input type="text" name="prenom">
+            <label>Nom</label>
+            <input type="text" name="nom">
+            <label>Adresse email</label>
+            <input type="email" name="email">
+            <label>Âge</label>
+            <input type="number" name="age">
+            <label>Filière souhaitée</label>
+            <select name="filiere">
+                <option value="">--Choisir--</option>
+                <option value="mathematiques">Mathématiques</option>
+                <option value="informatique">Informatique</option>
+                <option value="electronique">Electronique</option>
+                <option value="mecanique">Mécanique</option>
+                <option value="autre">Autre</option>
+            </select>
+            <label>Lettre de motivation</label>
+            <textarea name="motivation"></textarea>
+            <label>J'ai lu et j'accepte le règlement du club.</label>
+            <input type="checkbox" name="reglement" value="1">
+            <input type="submit" value="Envoyer ma candidature">
+        </form>
+    </div>
     
 </body>
 </html>
